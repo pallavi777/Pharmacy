2befabfb2efea749eb339acb1c6182ab5f3f27d6
pallavi.deshmukh <pallavi.deshmukh@Dev45.glodom.local>
2019-05-07 17:47:12 +0530

    updated

--- Pharmacy/src/main/java/triarq/qore/pharmacy/controller/PharmacyController.java
+++ Pharmacy/src/main/java/triarq/qore/pharmacy/controller/PharmacyController.java
@@ -2,7 +2,6 @@ package triarq.qore.pharmacy.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
